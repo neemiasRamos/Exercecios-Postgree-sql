23a8138915c076774c4f9c3436dc0c4b96eb6ae0
Neemias Ramos Ferreira <neemias.ferreira@fatec.sp.gov.br>
2019-07-29 10:24:20 -0300

    Create 2607.sql

---
+++ 2607.sql
@@ -0,0 +1,24 @@
+CREATE TABLE providers (
+  id numeric PRIMARY KEY,
+  name varchar(50),
+  street varchar(50),
+  city varchar(50),
+  state varchar(2)
+);
+
+INSERT INTO providers (id, name, street, city, state)
+VALUES 
+  (1,	'Henrique',	'Av Brasil',	'Rio de Janeiro',	'RJ'),
+  (2,	'Marcelo Augusto',	'Rua Imigrantes',	'Belo Horizonte',	'MG'),
+  (3,	'Caroline Silva',	'Av São Paulo',	'Salvador',	'BA'),
+  (4,	'Guilerme Staff',	'Rua Central',	'Porto Alegre',	'RS'),
+  (5,	'Isabela Moraes',	'Av Juiz Grande',	'Curitiba',	'PR'),
+  (6,	'Francisco Accerr',	'Av Paulista',	'São Paulo',	'SP');
+ 
+
+Questão
+Todos os meses a empresa pede um relatório das cidades que os fornecedores estão cadastrados. Dessa vez não vai ser diferente, faça uma consulta no Banco de Dados que retorne todas as cidades dos fornecedores, mas em ordem alfabética.
+OBS: Você não deve mostrar cidades repetidas.
+
+Resposta
+select distinct providers.city from providers order by providers.city asc;
