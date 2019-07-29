dddce54baa6d2cfe6a47969fee3b25cee434e667
Neemias Ramos Ferreira <neemias.ferreira@fatec.sp.gov.br>
2019-07-29 17:04:40 -0300

    Create ex-2625.sql

---
+++ ex-2625.sql
@@ -0,0 +1,37 @@
+CREATE TABLE customers (
+  id numeric PRIMARY KEY,
+  name varchar(255),
+  street varchar(255),
+  city varchar(255),
+  state char(2),
+  credit_limit numeric
+);
+
+CREATE TABLE natural_person (
+  id_customers numeric REFERENCES customers (id),
+  cpf char (14)
+);
+
+
+INSERT INTO customers (id, name, street, city, state, credit_limit)
+VALUES
+  (1,	'Nicolas Diogo Cardoso',	'Acesso Um',	'Porto Alegre',	'RS',	475),
+  (2,	'Cecília Olivia Rodrigues',	'Rua Sizuka Usuy',	'Cianorte',	'PR',	3170),
+  (3,	'Augusto Fernando Carlos Eduardo Cardoso',	'Rua Baldomiro Koerich',	'Palhoça',	'SC',	1067),
+  (4,	'Nicolas Diogo Cardoso',	'Acesso Um',	'Porto Alegre',	'RS',	475),
+  (5,	'Sabrina Heloisa Gabriela Barros',	'Rua Engenheiro Tito Marques Fernandes', 'Porto Alegre',	'RS',	4312),
+  (6,	'Joaquim Diego Lorenzo Araújo',	'Rua Vitorino',	'Novo Hamburgo',	'RS',	2314);
+  
+INSERT INTO natural_person (id_customers, cpf)
+VALUES
+  (1,	'26774287840'),
+  (2,	'97918477200');
+  
+
+Questão
+Os diretores do setor de comunicação da sua empresa querem um relatório sobre os dados dos clientes físicos que estão cadastrados no banco de dados. Porem o antigo relatório teve um problema. Os dados do CPF dos clientes vieram sem a máscara.
+Por isso seu trabalho agora é selecionar todos os CPFs de todos os clientes, e aplicar uma máscara sobre o retorno dos dados.
+A máscara do CPF é parecida com: '000.000.000-00'.
+
+Resposta
+select regexp_replace(cpf, '(\d{3})(\d{3})(\d{3})(\d{2})','\1.\2.\3-\4') from natural_person;
